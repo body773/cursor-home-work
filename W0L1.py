5c8bc2c3b7f88fa25b1018363f567197346f7838
body773 <body.nv@gmail.com>
2018-09-29 10:08:31 +0300

    Update W0L1.py

--- W0L1.py
+++ W0L1.py
@@ -1,59 +1,76 @@
-#Task 2.1
-my_list=[]
-i=11
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <title>Echoes - Free HTML Bootstrap Template</title>
+      <!-- Css -->
+      <link href="css/bootstrap.css" rel="stylesheet">
+      <link href="css/style.css" rel="stylesheet">
+      <script src="js/jquery.js"></script>
+   </head>
+   <body>
+      <nav class="navbar navbar-default color-fill navbar-fixed-top">
+         <div class="col-md-12">
+            <a class="navbar-brand" href="index.html">
+            <img class="logo" src="img/logo.png" alt="logo">
+            </a>
+         </div>
+      </nav>
+      <section id="about">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <h1>
+                    Homework "Introduction" (week 0/lesson 1)
+                  </h1>
+                  <br>
+               </div>
+            </div>
+            <br>
+            <div class="row margin-top-45">
+               <div class="col-md-3">
+                  <h2>#Task 2.1</h2>
+               </div>
+               <div class="col-md-9">
+                  <p>
+                    <code class="prettyprint lang-py">
+                        my_list=[]
+                           i=11
 k=1
 for i in range(1,i):
     my_list.append(k)
     k+=3
 print("Task 2.1 ",my_list)
+                        <script type="text/javascript">
 
-#Task 2.2
-my_listnew = my_list
-my_listnew.extend([1, 5, 13, 20])
-print ("Task 2.2 ",my_listnew)
-
-#Task2.3
-my_set = set(my_list)
-print("Task 2.3 ",my_set)
-
-#Task 2.4
-def compare_elements(a:list, b:set):
-    if len(a) > len(b):
-       return "List is bigger"
-    else:
-       return "Set is bigger"
-
-print("Task 2.4 ",compare_elements(my_listnew, my_set))
-
-#Task 3
-def get_value_from_list(data:list, index:int):
-    try:
-        return data[index]
-    except:
-        return "None"
-
-print("Task 3 ",get_value_from_list([1,2,3], 1))
-print("Task 3 ",get_value_from_list([1,2,3], 16))
-
-#Task 4
-dict = {"Name": "", "Age": "", "Hobby": ""}
-def create_dict(name:str, age:int, hobby:str):
-    dict["Name"] = name
-    dict["Age"] = age
-    dict["Hobby"] = hobby
-    return dict
-print("Task 4",create_dict("Denis", 26, "Books"))
-
-#Task 5
-
-def calculate_fibo(n:int):
-    fibo = [0]
-    a, b = 0, 1
-    for i in range(1,n,1):
-        fibo.append(b)
-        a, b = b, a + b
-    return fibo
-
-print(calculate_fibo(10))
-
-
+                           jQuery.get('hw1.py', function(data) {
+    print(data);
+});
+                        </script>
+                    </code>
+               </div>
+            </div>
+            <div class="row margin-top-45">
+               <div class="col-md-3">
+                  <h2>Services</h2>
+               </div>
+               <div class="col-md-9">
+                  <p>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vehicula libero auctor dolor vulputate varius. Nunc ac nisl non felis finibus ultricies. Donec at porttitor lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin volutpat augue ex. In hac habitasse platea dictumst. Suspendisse at ante ligula. Nulla facilisis velit egestas massa imperdiet, id posuere turpis feugiat.
+                  </p>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- script -->
+      <script src="js/jquery.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+      <script src="js/modernizr.js"></script>
+      <script src="js/script.js"></script>
+      <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
+   </body>
+</html>
